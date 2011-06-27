6a94b0ec05a44ba0a98116be0490c4b1507f0073
Reimar Grabowski <reimgrab@gmail.com>
2011-06-27 15:40:25 +0200

    Added package-info.java to the controller package.

---
+++ backend/grails-app/controllers/de/uni_koeln/arachne/dataservice/controller/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Provides the grails controller classes which handle the http requests RESTful.
+ * Request mappings are configured in the default package in UrlMappings.groovy.
+ */
+package de.uni_koeln.arachne.dataservice.controller;
