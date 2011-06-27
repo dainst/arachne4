25312032e01bd860731b9d2de0a88a1757dbf830
Reimar Grabowski <reimgrab@gmail.com>
2011-06-27 16:17:18 +0200

    Added documentation to ArachneEntity.groovy.

--- backend/grails-app/domain/de/uni_koeln/arachne/dataservice/domain/ArachneEntity.groovy
+++ backend/grails-app/domain/de/uni_koeln/arachne/dataservice/domain/ArachneEntity.groovy
@@ -1,13 +1,37 @@
 package de.uni_koeln.arachne.dataservice.domain
 
+/**
+ * 
+ * ORM class for the entity table (<code>arachneentityidentification</code>).
+ * 
+ */
 class ArachneEntity {
 	
-	int id
-	String tableName
-	int foreignKey
-	int isDeleted
-
+	/**
+	 * Primary key of the table (<code>ArachneEntityID</code>).
+	 */
+	int id;
 	
+	/**
+	 * Table name of the table the entity is located in (<code>TableName</code>).
+	 */
+	String tableName;
+	
+	/**
+	 * Foreign key of the table (<code>ForeignKey</code>).
+	 * This is the primary key of the table referenced in the <code>TableName</code> field.
+	 */
+	int foreignKey;
+	
+	/**
+	 * Field indicating if the referenced data record has been deleted (<code>isDeleted</code>).
+	 * This is used to keep URLs persistent even if the corresponding data record has been deleted.  
+	 */
+	int isDeleted;
+
+	/**
+	 * The actual object relational mapping. 
+	 */
 	static mapping = {
 		table 'arachneentityidentification'
 		id column: 'ArachneEntityID'
@@ -17,7 +41,9 @@ class ArachneEntity {
 		version false
 	}
 	
-	
+	/**
+	 * Not used.
+	 */
     static constraints = {
     }
 }
