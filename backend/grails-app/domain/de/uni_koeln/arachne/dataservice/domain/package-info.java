62af97e7166ae0e5588f130dc09d3a7f4a6e3ba3
Reimar Grabowski <reimgrab@gmail.com>
2011-06-27 15:47:05 +0200

    Added package-info.java to the domain package.

---
+++ backend/grails-app/domain/de/uni_koeln/arachne/dataservice/domain/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Provides the domain classes for ORM.
+ * Only the entity table ("arachneentityidentification") is object relationally mapped.  
+ */
+package de.uni_koeln.arachne.dataservice.domain;
