e625929efcb77b4f1a05b335f75996722f45d384
Reimar Grabowski <reimgrab@gmail.com>
2011-06-29 13:13:32 +0200

    Removed domain package-info.java. Preparation for moving all grails classes back to the default package.

--- backend/grails-app/domain/de/uni_koeln/arachne/dataservice/domain/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Provides the domain classes for ORM.
- * Only the entity table ("arachneentityidentification") is object relationally mapped.  
- */
-package de.uni_koeln.arachne.dataservice.domain;
