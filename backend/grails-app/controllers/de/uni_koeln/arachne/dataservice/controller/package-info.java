e9f8f431c34914263ea4485a1b176a118f2050ee
Reimar Grabowski <reimgrab@gmail.com>
2011-06-29 13:21:08 +0200

    Removed controller package-info.java. Preparation for moving all grails classes back to the default package.

--- backend/grails-app/controllers/de/uni_koeln/arachne/dataservice/controller/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Provides the grails controller classes which RESTfully handle the http requests.
- * Request mappings are configured in the default package in UrlMappings.groovy.
- */
-package de.uni_koeln.arachne.dataservice.controller;
