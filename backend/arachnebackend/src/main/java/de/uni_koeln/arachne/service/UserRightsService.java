9d949591164a3f9e516df19addb939eea0a3a6b0
Reimar Grabowski <>
2011-08-02 18:51:33 +0200

    Added documentation and changed source to be compliant with coding guidelines.(WIP)

--- backend/arachnebackend/src/main/java/de/uni_koeln/arachne/service/UserRightsService.java
+++ backend/arachnebackend/src/main/java/de/uni_koeln/arachne/service/UserRightsService.java
@@ -11,58 +11,64 @@ import de.uni_koeln.arachne.mapping.UserVerwaltung;
 
 @Service("UserRightsService")
 public class UserRightsService {
+	
+	/**
+	 * User management DAO instance.
+	 */
+	@Autowired
+	private UserVerwaltungDao userVerwaltungDao;
 
+	/**
+	 * Flag that indicates if the User Data is loaded.
+	 */
+	private boolean isSet = false;
 
-		@Autowired
-		private UserVerwaltungDao userVerwaltungDao;
+	/**
+	 * The Arachne user data set.
+	 */
+	private UserVerwaltung arachneUser = null;
 
+	/**
+	 * List of the groups the user has permissions to.
+	 */
+	//private List<String> userGroups;
+	private String userGroups;
 
-		
-		//Flag that initializeUserDatas if the User Data is loaded
-		private boolean isset = false;
-		// the Arachne user Dataset
-		private UserVerwaltung arachneUser = null;
-		//List of the userGroups the user has permissions on
-		//private List<String> userGroups;
-		private String userGroups;
-		
-		//The Informations about the user
-		private Map<String,String> userInfo ;
+	/**
+	 * Map holding informations about the user.
+	 */
+	private Map<String,String> userInfo;
 
-		/**
-		 * Private function to retrive the Session.
-		 * @return Session
-		 */
-		/*private HttpSession getSession() {
+	/**
+	 * Private function to retrive the Session.
+	 * @return Session
+	 */
+	/*private HttpSession getSession() {
 			return RequestContextHolder.currentRequestAttributes().getSession();
-		}*/
-		
-		/**
-		 * If the Userdata is not fetched yet,
-		 * Fetches the Username from the CAS-Ticket, 
-		 * gets the Database row with the User data and preformats it.
-		 * Else it does nothing.
-		 * @return VOID
-		 */
-	    private void initializeUserData() {
-			
-			if(! isset ){
-				//get username of validated CAS ticket
-				//String username = getSession().getAt(CASFilter.CAS_FILTER_USER);
-				
-				
-				
-				//String username = "rKrempel";
-				//get User Information from the Database
-				
-				 arachneUser =   userVerwaltungDao.findById( new Long( 695));
-				 
-				 //Splitting up all user permissions from the User dataset
-				 userGroups = arachneUser.getRightGroups();
-				 //Setting the user person information 
-				 //TODO maybe better as object
-				 userInfo = null;
-				 /*= [
+	}*/
+
+	/**
+	 * Method initializing access to the user data.
+	 * If the user data is not fetched yet, it fetches the user name from the CAS-Ticket, 
+	 * gets the database row with the user data and formats it.
+	 * Else it does nothing.
+	 */
+	private void initializeUserData() {
+		if (!isSet) {
+			//get username of validated CAS ticket
+			//String username = getSession().getAt(CASFilter.CAS_FILTER_USER);
+
+			//String username = "rKrempel";
+			//get User Information from the Database
+
+			arachneUser =   userVerwaltungDao.findById( new Long( 695));
+
+			//Splitting up all user permissions from the User dataset
+			userGroups = arachneUser.getRightGroups();
+			//Setting the user person information 
+			//TODO maybe better as object
+			userInfo = null;
+			/*= [
 					 "username":arachneUser.getUsername() ,
 					 "institution":arachneUser.getInstitution() ,
 					 "firstname":arachneUser.getFirstname(),
@@ -74,81 +80,75 @@ public class UserRightsService {
 					 "country":arachneUser.getCountry(),
 					 "telephone":arachneUser.getTelephone(),
 					 	  ]
-				 */
-				 isset = true;
-				
-			}
+			 */
+			isSet = true;
+		}
+	}		
 
-	    }		
-		
-		public String getUsername(){
-			initializeUserData();
-			return arachneUser.getUsername();
-			}
-		/**
-		 * Looks up all Group Rights the User has
-		 * @return returns a list of Strings with the User Groups the User is intothe user has
-		 */
-		public String getDataGroups(){
-			initializeUserData();
-			
-			return userGroups;
-			
-		}
-		
-		/**
-		 * Looks up if the User is allowed to see all Groups
-		 * @return true or false
-		 */
-		
-		public boolean isAuthorizedForAllGroups(){
-			initializeUserData();
-			
-			return arachneUser.isAll_groups();
-			
-		}
-		/**
-		 * is The User confirmed for login
-		 * @return true or false
-		 */
-		public boolean isConfirmed(){
-			initializeUserData();
-			
-			return arachneUser.isLogin_permission();
-			
-		}
-		/**
-		 * This Function initializeUserDatas if the User fullfils a specific clearence 
-		 * @param permissionNeeded The level of the permission minimal needed
-		 * @return boolean true or false
-		 */
-		public boolean isPermissionLevelValid(int permissionNeeded){
-			initializeUserData();
-			
-			if(permissionNeeded <= arachneUser.getGroupID() )
-				return true;
-			else
-				return false;
-			
-		}
-		/**
-		 * This function Collect all Data about the user
-		 * @return Key Value map with all the attributes of the User Info
-		 */
-		public Map getUserData(){
-			initializeUserData();
-			return userInfo;
-			
-		}
-		/**
-		 * Returns a Timestamp as String
-		 * @return Timestamp of last login
-		 */
-		public Date getLastLogin(){
-			initializeUserData();
-			return arachneUser.getLastLogin();
-		}
+	/**
+	 * Fetches the user name.
+	 * @return the user name as string. 
+	 */
+	public String getUsername(){
+		initializeUserData();
+		return arachneUser.getUsername();
+	}
+	
+	/**
+	 * Looks up all group rights of the user.
+	 * @return returns a list of strings with the groups the user is in.
+	 */
+	public String getDataGroups(){
+		initializeUserData();
+		return userGroups;
+	}
 
-		
-
+	/**
+	 * Looks up if the user is allowed to see all groups.
+	 * @return true or false
+	 */
+	public boolean isAuthorizedForAllGroups(){
+		initializeUserData();
+		return arachneUser.isAll_groups();
+	}
+	
+	/**
+	 * Returns if the user is confirmed for login.
+	 * @return true or false
+	 */
+	public boolean isConfirmed(){
+		initializeUserData();
+		return arachneUser.isLogin_permission();
+	}
+	
+	/**
+	 * Initializes user data if the user fulfills a specific clearance. 
+	 * @param permissionNeeded the minimum permission level needed
+	 * @return true or false
+	 */
+	public boolean isPermissionLevelValid(int permissionNeeded) {
+		initializeUserData();
+		if (permissionNeeded <= arachneUser.getGroupID())
+			return true;
+		else
+			return false;
+	}
+	
+	/**
+	 * This function collects all user data.
+	 * @return key/value-map with all the attributes of the user info
+	 */
+	public Map getUserData() {
+		initializeUserData();
+		return userInfo;
+	}
+	
+	/**
+	 * Returns the time of the last login.
+	 * @return timestamp as string
+	 */
+	public Date getLastLogin() {
+		initializeUserData();
+		return arachneUser.getLastLogin();
+	}
 }
