b7d2cfa66ab2145f84ebf4d534bed27f9ce7a0a9
Reimar Grabowski <>
2011-09-08 16:42:09 +0200

    Added ResponseFactory.

---
+++ backend/src/main/java/de/uni_koeln/arachne/responseobjects/ResponseFactory.java
@@ -0,0 +1,7 @@
+package de.uni_koeln.arachne.responseobjects;
+
+public class ResponseFactory {
+	public static FormattedArachneEntity createFormattedArachneEntity(ArachneDataset dataset) {
+		return null;
+	}
+}
