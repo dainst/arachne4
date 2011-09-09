fdff51b61c64db691fc7ceee9f7037d96728b324
Reimar Grabowski <>
2011-09-09 15:37:43 +0200

    Added base class for response objects.

---
+++ backend/src/main/java/de/uni_koeln/arachne/responseobjects/BaseArachneEntity.java
@@ -0,0 +1,42 @@
+package de.uni_koeln.arachne.responseobjects;
+
+public class BaseArachneEntity {
+	/**
+	 * Identification of the Dataset
+	 */
+	protected Long id;
+	
+	/**
+	 * The tablename field of the ArachneEntity table
+	 */
+	protected String type;
+	
+	/**
+	 * The foreignKey field of the ArachneEntity table
+	 */
+	protected Long internalId;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public Long getInternalId() {
+		return internalId;
+	}
+
+	public void setInternalId(Long internalId) {
+		this.internalId = internalId;
+	}
+}
