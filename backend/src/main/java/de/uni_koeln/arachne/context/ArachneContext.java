9dd66d27b579c7ccc28b41b580faacb600c692ef
Reimar Grabowski <>
2011-09-29 15:42:07 +0200

    Refactoring and documentation update.

--- backend/src/main/java/de/uni_koeln/arachne/context/ArachneContext.java
+++ backend/src/main/java/de/uni_koeln/arachne/context/ArachneContext.java
@@ -1,5 +1,6 @@
 package de.uni_koeln.arachne.context;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,19 +8,39 @@ import org.springframework.beans.factory.annotation.Autowired;
 import de.uni_koeln.arachne.response.ArachneDataset;
 import de.uni_koeln.arachne.response.Link;
 import de.uni_koeln.arachne.service.ArachneContextService;
+
 /**
- * This Class is a Wrapper that Holds and Manages Contexts of Datasets. The links are fetched on Demand.
- * The Combination of Context Name and Parent describes the two ends of the Links. 
+ * This Class is a Wrapper that holds and manages contexts of <code>ArachneDatasets</code>. The links are fetched on demand.
+ * The combination of context name and parent describes the two ends of a <code>Link</code>. 
  */
 public class ArachneContext {
-	// A Specified Context Type like Ort, Literatur, Literaturzitat.
-	private String contextName;
+	/**
+	 * @param ctname The Name of the Contexts this class Manages
+	 * @param par The ArachneDataset this Context belongs to
+	 */
+	public ArachneContext(String ctname, ArachneDataset parent) {
+		completionState = CompletionStateEnum.EMPTY;
+		contextType = ctname;
+		this.parent = parent;
+		contextEntities = new ArrayList<Link>();
+	}
+	
+	/**
+	 *  A specified context type like Ort, Literatur, Literaturzitat.
+	 *  In most cases the context type is the name of the table used for the query.
+	 *  For external contexts (not implemented yet) a different scheme is used.
+	 */
+	protected String contextType;
 
-	//The Parent Dataset, it Describes where The Contexts belongs to.
-	private ArachneDataset parent;
+	/**
+	 * The parent dataset. It describes where the contexts belongs to.
+	 */
+	protected ArachneDataset parent;
 
-	//An Enumeration which Represents the State of the Context
-	private enum CompletionVersionEnum {
+	/**
+	 * An enumeration class representing the state of the context.
+	 */
+	protected enum CompletionStateEnum {
 		//Full means that the Context is Completely loaded
 		FULL, 
 		//Limited means that the Context contains more than one context but is not Complete
@@ -30,82 +51,99 @@ public class ArachneContext {
 		EMPTY
 	};
 	
-	private CompletionVersionEnum completionVersion;
+	/**
+	 * The completion state of the context. Can be one of <code>FULL</code>
+	 */
+	protected CompletionStateEnum completionState;
 		
-	//The Autowired Service which manges the Retrival etc.
+	/**
+	 * The Autowired Service which manages the Retrival etc.
+	 */
 	@Autowired
-	private ArachneContextService cs;
-	
-	//Depth of The Context
-	private int depthLevel;
-
-	//List of Links
-	List<Link> contextEntities;
+	protected ArachneContextService contextService;
 	
 	/**
-	 * @param ctname The Name of the Contexts this class Manages
-	 * @param par The ArachneDataset this Context belongs to
+	 * The depth of the context.
 	 */
-	public ArachneContext(String ctname,ArachneDataset par) {
-		completionVersion = CompletionVersionEnum.EMPTY;
-		contextName = ctname;
-		parent = par;
-	}
+	protected int depthLevel;
+
+	/**
+	 * This list of <code>Link</code> contains the data of the context.
+	 */
+	public List<Link> contextEntities;
 	
 	//The Context Getter
+	
 	/**
-	 * Return every Link in this Context
-	 * @return The Complete List of Contexts 
+	 * Return every <code>Link</code> in this context.
+	 * @return The complete list of contexts.
 	 */
 	public List<Link> getallContexts() {
-	if (completionVersion != CompletionVersionEnum.FULL)
-	    doCompleteRetrival();
+	if (completionState != CompletionStateEnum.FULL)
+	    retrieveComplete();
 		return contextEntities;
 	}
 
 	/**
-	 * Returns the First Link Element of the Context
-	 * @return The First Link Element of the Context
+	 * Returns the first link element of the context.
+	 * @return The First <code>Link</code> of the context.
 	 */
 	public Link getFirstContext() {
-	if (completionVersion != CompletionVersionEnum.EMPTY)
-	    doFirstRetrival();
+	if (completionState != CompletionStateEnum.EMPTY)
+	    retrieveFirst();
 		return contextEntities.get(0);
 	}
+	
 	/**
-	 * return a given number of Contexts or the Maximum Number of Contexts
-	 * @param howMany The Number of Contexts Demanded
-	 * @return The number of Contexts More or less
+	 * Return a given number of contexts or the maximum number of contexts.
+	 * @param number The number of contexts demanded.
+	 * @return The number of contexts (more or less).
 	 */
-	public List<Link> getLimitContext(int howMany) {
-	if (completionVersion != CompletionVersionEnum.LIMITED && completionVersion != CompletionVersionEnum.FULL)
-	    doLimitRetrival(howMany);
-	    if (howMany > contextEntities.size())
-	        completionVersion = CompletionVersionEnum.FULL;
+	public List<Link> getLimitContext(int number) {
+	if (completionState != CompletionStateEnum.LIMITED && completionState != CompletionStateEnum.FULL)
+	    retrieveLimited(number);
+	    if (number > contextEntities.size())
+	        completionState = CompletionStateEnum.FULL;
 	    else
-	        completionVersion = CompletionVersionEnum.LIMITED;
+	        completionState = CompletionStateEnum.LIMITED;
 
 	    return contextEntities;
 	}
 	
-	//The Retrival Functions are Wrappers for the doRetrival method
-	protected void doCompleteRetrival() {
-	    doRetrival(contextEntities.size(), -1);
-	    completionVersion = CompletionVersionEnum.FULL;
+	/**
+	 * Internally used convenient function to fill the context list with all available contexts.
+	 * Side effect: Sets <code>completionState</code>.
+	 */
+	protected void retrieveComplete() {
+	    retrieve(contextEntities.size(), -1);
+	    completionState = CompletionStateEnum.FULL;
 	}
 
-	protected void doFirstRetrival() {
-	    doRetrival(0, 1);
-	    completionVersion = CompletionVersionEnum.FIRST;
+	/**
+	 * Internally used convenient function to only retrieve the first context.
+	 * Side effect: Sets <code>completionState</code>
+	 */
+	protected void retrieveFirst() {
+	    retrieve(0, 1);
+	    completionState = CompletionStateEnum.FIRST;
 	}
 
-	protected void doLimitRetrival(int howMany) {
-	    doRetrival(contextEntities.size(), howMany - contextEntities.size());
-	    completionVersion = CompletionVersionEnum.LIMITED;
+	/**
+	 * Internally used convenient function to retrieve a limited number of contexts.
+	 * Side effects: Sets <code>completionState</code>
+	 * @param number The number of contexts to retrieve
+	 */
+	protected void retrieveLimited(int number) {
+	    retrieve(contextEntities.size(), number - contextEntities.size());
+	    completionState = CompletionStateEnum.LIMITED;
 	}
 
-	//Do Retrival Calls the Context Service with the needed Information
-	protected void doRetrival(int offset, int Limit) {
+	/**
+	 * Internally used function to call the context service with the needed information to fill the <code>contextEntities</code>.
+	 * @param offset And offset describing where to start getting context information.
+	 * @param limit The maximum number of contexts to retrieve.
+	 */
+	protected void retrieve(int offset, int Limit) {
 	    //List<Link> temporary = CS.getLinks(parent, contextName, offset, limit);
 	    //contextEntities.appendAll(temporary);
 	}
