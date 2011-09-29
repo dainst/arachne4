1349d562d7a2dedc37494b876d7423a8d2112819
krempel r.Krempel@gmx.de <>
2011-09-29 13:11:41 +0200

    Some Changes in The method permissions and documentation added

--- backend/src/main/java/de/uni_koeln/arachne/service/Contextualizer.java
+++ backend/src/main/java/de/uni_koeln/arachne/service/Contextualizer.java
@@ -4,9 +4,23 @@ import java.util.List;
 
 import de.uni_koeln.arachne.response.ArachneDataset;
 import de.uni_koeln.arachne.response.Link;
-
+/**
+ * This Interface Describes the Minimum Functions a Context retrivers must Implement
+ * 
+ */
 public interface Contextualizer {
-	abstract String getContextType();
-	abstract List<Link> retrive(ArachneDataset Source, Integer offset, Integer limit);
+	/**
+	 * Returns the Type of Context this Contextualizer serves
+	 * @return String that Describes the Context Tool Serves example "ort"
+	 */
+	public String getContextType();
+	/**
+	 * 
+	 * @param Source The Dataset form which the Context is Created
+	 * @param Offset The Start of the Stuff
+	 * @param limit The Number of Contextes to get
+	 * @return The Links that Represent the Context
+	 */
+	public List<Link> retrive(ArachneDataset Source, Integer offset, Integer limit);
 
 }
