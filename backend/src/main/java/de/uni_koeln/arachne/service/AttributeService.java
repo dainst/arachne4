1964900e15116dfe59b9871eab435d13984f2f15
Reimar Grabowski <reimgrab@gmail.com>
2011-10-13 19:10:24 +0200

    Refactoring and documentation.

--- backend/src/main/java/de/uni_koeln/arachne/service/AttributeService.java
+++ backend/src/main/java/de/uni_koeln/arachne/service/AttributeService.java
@@ -20,6 +20,10 @@ import de.uni_koeln.arachne.response.ArachneDataset;
 import de.uni_koeln.arachne.response.Content;
 import de.uni_koeln.arachne.sqlutil.ArachneSQLToolbox;
 
+/**
+ * This service retrieves the external fields that are declared inside the corresponding
+ * xml configuration file.
+ */
 @Service("attributeService")
 public class AttributeService {
 
@@ -29,12 +33,20 @@ public class AttributeService {
 	@Autowired
 	private ServletContext servletContext;
 
+	/**
+	 * Used to fetch values from the database via dynamic SQL.
+	 */
 	@Autowired
 	GenericFieldService genericFieldService;
 	
+	/**
+	 * Adds the external fields by full qualified name and their respective values to the datasets
+	 * map of fields.
+	 * @param dataset The <code>ArachneDataset</code> to add the fields/values to.
+	 */
 	public void addExternalFields(ArachneDataset dataset) {
 		Map<String, String> externalFieldMap = new HashMap<String, String>();
-		List<String> externalFields = getExternalFields(dataset);
+		List<String> externalFields = getExternalFields(dataset.getArachneId().getTableName());
 		String tableName = dataset.getArachneId().getTableName();
 		for (String currentField : externalFields) {
 			String[] tableAndField = currentField.split("\\.", 2);
@@ -42,7 +54,7 @@ public class AttributeService {
 					, dataset.getArachneId().getInternalKey(), tableAndField[1]);
 			if (queryResult != null) {
 				if (!queryResult.isEmpty()) {
-					System.out.println(currentField + "Query: " + queryResult.get(0));
+					System.out.println("Field: " + currentField + " Query: " + queryResult.get(0) + "(" + queryResult.isEmpty() + ")");
 					// TODO handle multiple values
 					externalFieldMap.put(currentField, queryResult.get(0));
 				}
@@ -52,8 +64,14 @@ public class AttributeService {
 		dataset.appendFields(externalFieldMap);
 	}
 	
-	private List<String> getExternalFields(ArachneDataset dataset) {	
-		String filename = "/WEB-INF/xml/"+ dataset.getArachneId().getTableName() + ".xml";
+	/**
+	 * Internally used method to get the external field names from the XML files.
+	 * It opens the XML file, creates the DOM and uses <code>getFields</code> to get the values from the dom.
+	 * @param type The of the xml file.
+	 * @return A list of full qualified external field names.
+	 */
+	private List<String> getExternalFields(String type) {	
+		String filename = "/WEB-INF/xml/"+ type + ".xml";
 
 		ServletContextResource xmlDocument = new ServletContextResource(servletContext, filename);
 		try {
@@ -63,7 +81,7 @@ public class AttributeService {
 			Element display = doc.getRootElement().getChild("display");
 
 			List<String> result = new ArrayList<String>();
-			result.addAll(getFields(display, dataset.getArachneId().getTableName()));
+			result.addAll(getFields(display, type));
 			return result;		
 		} catch (JDOMException e) {
 			// TODO Auto-generated catch block
@@ -75,6 +93,12 @@ public class AttributeService {
 		return null;
 	}
 	
+	/**
+	 * Recursive function adding all external fields of an <code>Element</code> and its children to the result list.
+	 * @param element The XML node to check for external fields.
+	 * @param parentType The type of the <code>ArachneDataset</code>.
+	 * @return A list of full qualified external field names.
+	 */
 	private List<String> getFields(Element element, String parentType) {
 		List<String> result = new ArrayList<String>();
 		@SuppressWarnings("unchecked")
