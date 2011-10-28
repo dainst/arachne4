8262b047d8eab1c493b7aa9ee0e98cb3c98da361
Reimar Grabowski <>
2011-10-28 16:31:30 +0200

    Added base class LeftJoinTableContextualizer.

--- backend/src/main/java/de/uni_koeln/arachne/context/LiteraturContextualizer.java
+++ backend/src/main/java/de/uni_koeln/arachne/context/LiteraturContextualizer.java
@@ -1,76 +1,13 @@
 package de.uni_koeln.arachne.context;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.ListIterator;
-
-import de.uni_koeln.arachne.response.ArachneDataset;
 import de.uni_koeln.arachne.service.ArachneEntityIdentificationService;
 import de.uni_koeln.arachne.service.GenericSQLService;
-import de.uni_koeln.arachne.util.ArachneId;
 
-public class LiteraturContextualizer implements IContextualizer {
-	
-	private ArachneEntityIdentificationService arachneEntityIdentificationService;
-	private GenericSQLService genericFieldService;
-	/**
-	 * constructor
-	 */
-	public LiteraturContextualizer(ArachneEntityIdentificationService arachneEntityIdentificationService, GenericSQLService genericFieldService) {
-		this.arachneEntityIdentificationService = arachneEntityIdentificationService;
-		this.genericFieldService = genericFieldService;
-		
+public class LiteraturContextualizer extends LeftJoinTableContextualizer {
+	public LiteraturContextualizer(ArachneEntityIdentificationService arachneEntityIdentificationService
+			, GenericSQLService genericSQLService) {
+		super(arachneEntityIdentificationService, genericSQLService);
+		tableName = "literatur";
+		joinTableName = "literaturzitat_leftjoin_literatur";
 	}
-	
-	@Override
-	public String getContextType() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	private Long linkCount = 0l;
-	
-	@Override
-	public List<Link> retrieve(ArachneDataset parent, Integer offset, Integer limit) {
-		List<Link> result = new ArrayList<Link>();
-		String parentTableName = parent.getArachneId().getTableName();
-		
-		List<Long> contextIds = genericFieldService.getIdByFieldId("literaturzitat_leftjoin_literatur", parentTableName, parent.getArachneId().getInternalKey(), "literatur");
-		
-		if (contextIds != null) {
-			ListIterator<Long> contextId = contextIds.listIterator(offset);
-			while (contextId.hasNext() && linkCount < limit) {
-				ArachneLink link = new ArachneLink();
-					//Performance for testing
-					ArachneDataset aDs = new ArachneDataset();
-				ArachneId id = arachneEntityIdentificationService.getId("Literatur", contextId.next());
-				link.setEntity1(parent);
-				//for performance changed to empty dataset ONLY with arachneId
-				//link.setEntity2(arachneSingleEntityDataService.getSingleEntityByArachneId(id));
-					//performance testing
-					aDs.setArachneId(id);
-					link.setEntity2(aDs);
-				linkCount += 1;
-				System.out.println("Adding Link Literatur" + " number " + linkCount + "/" + limit + " of " + contextIds.size());
-				result.add(link);
-			}
-		}
-		return result;
-	}
-	
-	
-	/*
-	 * SETTER
-	 */
-	
-	public void setArachneEntityIdentificationService(
-			ArachneEntityIdentificationService arachneEntityIdentificationService) {
-		this.arachneEntityIdentificationService = arachneEntityIdentificationService;
-	}
-
-
-
-	public void setGenericFieldService(GenericSQLService genericFieldService) {
-		this.genericFieldService = genericFieldService;
-	}
-
 }
--- backend/src/main/java/de/uni_koeln/arachne/context/OrtContextualizer.java
+++ backend/src/main/java/de/uni_koeln/arachne/context/OrtContextualizer.java
@@ -1,71 +1,13 @@
 package de.uni_koeln.arachne.context;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.ListIterator;
-import java.util.Map;
-
-import de.uni_koeln.arachne.response.ArachneDataset;
 import de.uni_koeln.arachne.service.ArachneEntityIdentificationService;
 import de.uni_koeln.arachne.service.GenericSQLService;
 
-import de.uni_koeln.arachne.util.ArachneId;
-
-public class OrtContextualizer implements IContextualizer {
-
-	private ArachneEntityIdentificationService arachneEntityIdentificationService;
-	private GenericSQLService genericSQLService;
-	/**
-	 * Constructor setting the needed services.
-	 */
+public class OrtContextualizer extends LeftJoinTableContextualizer {
 	public OrtContextualizer(ArachneEntityIdentificationService arachneEntityIdentificationService
 			, GenericSQLService genericSQLService) {
-		this.arachneEntityIdentificationService = arachneEntityIdentificationService;
-		this.genericSQLService = genericSQLService;
-	}
-	
-	@Override
-	public String getContextType() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
-	private Long linkCount = 0l;
-	
-	@Override
-	public List<Link> retrieve(ArachneDataset parent, Integer offset, Integer limit) {
-		List<Link> result = new ArrayList<Link>();
-		String parentTableName = parent.getArachneId().getTableName();
-		System.out.println("OrtContextualizer getting contexts...");
-		List<Map<String, String>> contextContents = genericSQLService.getEntitiesById("ortsbezug_leftjoin_ort"
-				, parentTableName, parent.getArachneId().getInternalKey());
-		
-		if (contextContents != null) {
-			ListIterator<Map<String, String>> contextMap = contextContents.listIterator(offset);
-			while (contextMap.hasNext() && linkCount < limit) {
-				Map<String, String> map = contextMap.next();
-				ArachneLink link = new ArachneLink();
-				ArachneDataset dataset = new ArachneDataset();
-				String id = map.get("ortsbezug_leftjoin_ort.PS_OrtID");
-				ArachneId arachneId = arachneEntityIdentificationService.getId("Ort", Long.parseLong(id));
-				dataset.setArachneId(arachneId);
-				// rename ortsbezug_leftjoin_ort to ort
-				// this is how the contextualizer can set his own names
-				Map<String, String> resultMap = new HashMap<String, String>();
-				for (Map.Entry<String, String> entry: map.entrySet()) {
-					String key = entry.getKey();
-					if (!(key.contains("PS_") && key.contains("ID"))) {
-						String newKey = "ort." + key.split("\\.")[1];
-						resultMap.put(newKey, entry.getValue());
-					}
-				}
-				dataset.appendFields(resultMap);
-				link.setEntity1(parent);
-				link.setEntity2(dataset);
-				result.add(link);
-			}
-		}
-		return result;
+		super(arachneEntityIdentificationService, genericSQLService);
+		tableName = "ort";
+		joinTableName = "ortsbezug_leftjoin_ort";
 	}
 }
