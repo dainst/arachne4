7a34ee9e6f157d9984e58d459ef108ea89f2bf85
Reimar Grabowski <>
2012-01-10 16:49:52 +0100

    Implemented basic search.

--- backend/src/main/java/de/uni_koeln/arachne/controller/SearchController.java
+++ backend/src/main/java/de/uni_koeln/arachne/controller/SearchController.java
@@ -1,5 +1,13 @@
 package de.uni_koeln.arachne.controller;
 
+import java.net.MalformedURLException;
+
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrServer;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.CommonsHttpSolrServer;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrDocumentList;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,9 +27,29 @@ public class SearchController {
      * @return A response object containing the data (this is serialized to XML or JSON depending on content negotiation).
      */
 	@RequestMapping(value="/search", method=RequestMethod.GET)
-	public @ResponseBody SearchResult handleSearchRequest(@RequestParam("q") String searchParam) {
-		SearchResult result = new SearchResult();
-		result.result = searchParam;
-		return result;
+	public @ResponseBody SolrDocumentList handleSearchRequest(@RequestParam("q") String searchParam) {
+		//SearchResult result = new SearchResult();
+		SolrDocumentList result = null;
+		String url = "http://crazyhorse.archaeologie.uni-koeln.de:8080/solr3.4.0/";
+		SolrServer server = null;
+		try {
+			server = new CommonsHttpSolrServer(url);
+			SolrQuery query = new SolrQuery();
+		    query.setQuery(searchParam);
+		    query.setRows(1000);
+		    query.setShowDebugInfo(true);
+		    //query.addSortField( "price", SolrQuery.ORDER.asc );
+		    QueryResponse rsp = server.query(query);
+		    //SolrDocumentList docs = rsp.getResults();
+		    result = rsp.getResults();
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SolrServerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+			    
+	    return result;
 	}
 }
