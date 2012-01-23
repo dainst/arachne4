a3b015e2d66f31bc89c627edf704c169cbbfe859
Reimar Grabowski <reimgrab@gmail.com>
2012-01-23 16:59:16 +0100

    Added isEmptyOrNull method for type List<String>.

--- backend/src/main/java/de/uni_koeln/arachne/util/StrUtils.java
+++ backend/src/main/java/de/uni_koeln/arachne/util/StrUtils.java
@@ -1,5 +1,7 @@
 package de.uni_koeln.arachne.util;
 
+import java.util.List;
+
 /**
  * Class implementing string utility functions.
  */
@@ -12,6 +14,22 @@ public class StrUtils {
 	public static boolean isEmptyOrNull(String string) {
 		if (string != null) {
 			if (string.isEmpty()) {
+				return true;
+			}
+			return false;
+		} else {
+			return true;
+		}
+	}
+	
+	/**
+	 * This method returns if a <code>List<String></code> is empty or <code>null</code>.
+	 * @param stringlist to test.
+	 * @return <code>true</code> or <code>false</code>
+	 */
+	public static boolean isEmptyOrNull(List<String> stringList) {
+		if (stringList != null) {
+			if (stringList.isEmpty()) {
 				return true;
 			}
 			return false;
