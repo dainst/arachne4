04e0190c3a28523d764a9a9736e2df85cc053bc9
Reimar Grabowski <reimgrab@gmail.com>
2012-01-25 14:11:13 +0100

    Added support for multiple filter queries.

--- backend/src/main/java/de/uni_koeln/arachne/controller/SearchController.java
+++ backend/src/main/java/de/uni_koeln/arachne/controller/SearchController.java
@@ -1,8 +1,7 @@
 package de.uni_koeln.arachne.controller;
 
 import java.net.MalformedURLException;
-import java.sql.Struct;
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -13,8 +12,6 @@ import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.impl.CommonsHttpSolrServer;
 import org.apache.solr.client.solrj.response.FacetField;
 import org.apache.solr.client.solrj.response.QueryResponse;
-import org.apache.solr.common.SolrDocumentList;
-import org.apache.solr.common.params.FacetParams;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -30,13 +27,16 @@ import de.uni_koeln.arachne.util.StrUtils;
 @Controller
 public class SearchController {
 	/**
-	 * Handles the http request.
-	 *  
+	 * Handles the http request by querying the Solr index and returning the search result.
 	 * <br>
 	 * Currently the search result can only be serialized to JSON as JAXB cannot handle Maps.
-	 * @param searchParam The value of the search parameter.
-     * @return A response object containing the data (this is serialized to XML or JSON depending on content negotiation).
-     */
+	 * @param searchParam The value of the search parameter. (mandatory)
+	 * @param limit The maximum number of returned entities. (optional)
+	 * @param offset The offset into the list of entities (used for paging). (optional)
+	 * @param filterValues The values of the solr filter query. (optional)
+	 * @param facetLimit The maximum number of facet results. (optional)
+	 * @return A response object containing the data (this is serialized to XML or JSON depending on content negotiation).
+	 */
 	@RequestMapping(value="/search", method=RequestMethod.GET)
 	public @ResponseBody SearchResult handleSearchRequest(@RequestParam("q") String searchParam,
 														  @RequestParam(value = "limit", required = false) String limit,
@@ -75,9 +75,12 @@ public class SearchController {
 		    	query.setFacetLimit(intFacetLimit);
 		    }
 		    if (!StrUtils.isEmptyOrNull(filterValues)) {
-		    	// TODO remove debug
-		    	System.out.println("fq: " + filterValues);
-		    	query.addFilterQuery(filterValues);
+		    	List<String> filterValueList = filterQueryStringToStringList(filterValues); 
+		    	if (!StrUtils.isEmptyOrNull(filterValueList)) {
+		    		for (String filterValue: filterValueList) {
+		    			query.addFilterQuery(filterValue);
+		    		}
+		    	}
 		    }
 		    		    
 		    QueryResponse response = server.query(query);
@@ -110,5 +113,28 @@ public class SearchController {
 		}
 			    
 	    return result;
+	}
+	
+	/**
+	 * Converts the input string of query filter parameters to a string list of parameters.
+	 * The string is split at every occurence of ",facet_".
+	 * @param string The silter query string to convert.
+	 * @return a string list containing the seperated parameters or <code>null</code> if the conversion fails.
+	 */
+	private List<String> filterQueryStringToStringList(String string) {
+		if (string.startsWith("facet_")) {
+			List<String> result = new ArrayList<String>();
+			int index = string.indexOf(",facet_");
+			while (index != -1) {
+				String subString = string.substring(0, index);
+				string = string.substring(index + 1);
+				index = string.indexOf(",facet_");
+				result.add(subString);
+			}
+			result.add(string);
+			return result;
+		} else {
+			return null;
+		}
 	}
 }
