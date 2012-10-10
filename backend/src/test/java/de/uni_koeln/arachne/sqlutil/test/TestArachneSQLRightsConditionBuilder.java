e2de40dbf4b63c17ebb2abab2a6a5815fdcdcfc2
Reimar Grabowski <>
2012-10-10 15:47:50 +0200

    Fixed TestArachneSQLRightsConditionBuilder.

--- backend/src/test/java/de/uni_koeln/arachne/sqlutil/test/TestArachneSQLRightsConditionBuilder.java
+++ backend/src/test/java/de/uni_koeln/arachne/sqlutil/test/TestArachneSQLRightsConditionBuilder.java
@@ -1,10 +1,6 @@
 package de.uni_koeln.arachne.sqlutil.test;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-
-import java.util.HashSet;
-import java.util.Set;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,13 +11,9 @@ import org.springframework.test.context.support.DependencyInjectionTestExecution
 import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
 import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
 
-import de.uni_koeln.arachne.mapping.DatasetGroup;
-import de.uni_koeln.arachne.mapping.UserAdministration;
 import de.uni_koeln.arachne.sqlutil.SQLRightsConditionBuilder;
 import de.uni_koeln.arachne.test.WebContextTestExecutionListener;
 
-
-//TODO rewrite test to work with new SQLRightsConditionBuilder
 @RunWith(SpringJUnit4ClassRunner.class) 
 @ContextConfiguration(locations={"classpath:test-context.xml"}) 
 @TestExecutionListeners( { WebContextTestExecutionListener.class,
@@ -32,30 +24,9 @@ public class TestArachneSQLRightsConditionBuilder {
 	
 	@Test
 	public void testArachneSQLRightsConditionBuilder(){
-		final UserAdministration user = new UserAdministration();
-		final Set<DatasetGroup> set = new HashSet<DatasetGroup>();
-		set.add(new DatasetGroup("Arachne"));
-		user.setDatasetGroups(set);
-		
-		
 		SQLRightsConditionBuilder rcb = new SQLRightsConditionBuilder("bauwerk");
-		
-		assertEquals(rcb.getUserRightsSQLSnipplett(), " AND ( `bauwerk`.`DatensatzGruppeBauwerk` = \"Arachne\")");
-		
-		set.add(new DatasetGroup("Oppenheim"));
-		
-		rcb = new SQLRightsConditionBuilder("bauwerk");
 		
 		assertTrue(rcb.getUserRightsSQLSnipplett().contains("`bauwerk`.`DatensatzGruppeBauwerk` = \"Arachne\""));
 		assertTrue(rcb.getUserRightsSQLSnipplett().contains("`bauwerk`.`DatensatzGruppeBauwerk` = \"Oppenheim\""));
-		
-		user.setAll_groups(true);
-		
-		rcb = new SQLRightsConditionBuilder("bauwerk");
-		
-		assertEquals(rcb.getUserRightsSQLSnipplett(), "");		
-		
-		assertEquals(true, true);
-	}
-	
+	}	
 }
