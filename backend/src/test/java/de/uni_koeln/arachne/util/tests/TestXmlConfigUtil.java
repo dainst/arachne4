a5b979684034ad68412de27a4a17dcfa20c2e233
Reimar Grabowski <reimgrab@gmail.com>
2012-10-30 17:52:30 +0100

    Updated XmlConfigUtil test.

--- backend/src/test/java/de/uni_koeln/arachne/util/tests/TestXmlConfigUtil.java
+++ backend/src/test/java/de/uni_koeln/arachne/util/tests/TestXmlConfigUtil.java
@@ -1,9 +1,12 @@
 package de.uni_koeln.arachne.util.tests;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.mock.web.MockServletContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -21,9 +24,36 @@ import de.uni_koeln.arachne.util.XmlConfigUtil;
 	DirtiesContextTestExecutionListener.class,
 	TransactionalTestExecutionListener.class })
 public class TestXmlConfigUtil {
+	private transient XmlConfigUtil xmlConfigUtil;
+	
+	@Before
+	public void setUp() {
+		xmlConfigUtil = new XmlConfigUtil();
+		xmlConfigUtil.setServletContext(new MockServletContext("file:src/main/webapp"));
+	}
+	
 	@Test
-	public void testXmlConfigUtil() {
-		final XmlConfigUtil xmlConfigUtil = new XmlConfigUtil();
-		assertNotNull(xmlConfigUtil);
+	public void testGetDocument() {
+		assertNotNull(xmlConfigUtil.getDocument("bauwerk"));
+		assertNotNull(xmlConfigUtil.getDocument("bauwerksteil"));
+		assertNotNull(xmlConfigUtil.getDocument("buch"));
+		assertNotNull(xmlConfigUtil.getDocument("buchseite"));
+		assertNotNull(xmlConfigUtil.getDocument("gruppen"));
+		assertNotNull(xmlConfigUtil.getDocument("inschrift"));
+		assertNotNull(xmlConfigUtil.getDocument("literatur"));
+		assertNotNull(xmlConfigUtil.getDocument("marbilder"));
+		assertNotNull(xmlConfigUtil.getDocument("objekt"));
+		assertNotNull(xmlConfigUtil.getDocument("ort"));
+		assertNotNull(xmlConfigUtil.getDocument("person"));
+		assertNotNull(xmlConfigUtil.getDocument("realien"));
+		assertNotNull(xmlConfigUtil.getDocument("relief"));
+		assertNotNull(xmlConfigUtil.getDocument("reproduktion"));
+		assertNotNull(xmlConfigUtil.getDocument("sammler"));
+		assertNotNull(xmlConfigUtil.getDocument("sammlungen"));
+		assertNotNull(xmlConfigUtil.getDocument("sarkophag"));
+		assertNotNull(xmlConfigUtil.getDocument("topographie"));
+		assertNotNull(xmlConfigUtil.getDocument("typus"));
+		
+		assertNull(xmlConfigUtil.getDocument("unknowntype"));
 	}
 }
