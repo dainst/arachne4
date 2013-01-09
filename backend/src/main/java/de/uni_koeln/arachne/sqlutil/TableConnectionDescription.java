c7befcdb92a0bf679d9e3a180f18492a5a326118
Rasmus Krempel <r.Krempel@uni-koeln.de>
2013-01-09 17:41:27 +0100

    Commented Work

--- backend/src/main/java/de/uni_koeln/arachne/sqlutil/TableConnectionDescription.java
+++ backend/src/main/java/de/uni_koeln/arachne/sqlutil/TableConnectionDescription.java
@@ -1,7 +1,15 @@
 package de.uni_koeln.arachne.sqlutil;
 /**
- * This Object Describes a Connection by a Coomeaning of heit fields
- *Example
+ * This Object Describes a Connection between two tables by Describing the Fields That are Equal in Both Tables
+ *
+ *Example:
+ *objekt<->objektplastik
+ *Here PS_ObjektID Equals PS_ObjektplastikID
+ *This Describes the Link between Objekts and Their Subclasses! 
+ *table1 = objekt
+ *table2 = objektplastik
+ *field1 = PS_ObjektID
+ *field2 = PS_ObjektplastikID
  */
 public class TableConnectionDescription {
 	//Name of The first Table
@@ -55,13 +63,17 @@ public class TableConnectionDescription {
 	/**
 	 * Checks for the tablename if one of the Things Lists this
 	 * @param tableName The name of The Table to check
-	 * @return true if tbname is described in this connection
+	 * @return true if tableName is described in this connection
 	 */
 	public boolean linksTable(final String tableName){
 		return tableName.equals(table1) || tableName.equals(table2);
 	}
 	
 	//Setter
+	/**
+	 * This Sets the Name of the Cross table which is used to Link the two Tables
+	 * @param crosstableName Name of the Crosstable that Links two Tables
+	 */
 	public void setCrosstableName(final String crosstableName) {
 		this.crossTableName = crosstableName;
 		if(crosstableName == null || crosstableName.isEmpty()){
@@ -70,15 +82,33 @@ public class TableConnectionDescription {
 			connectionByCrosstable = true;
 		}	
 	}
+	/**
+	 * Set Field of the First table That is Desricbed
+	 * @param field1 Field Name
+	 */
 	public void setField1(final String field1) {
 		this.field1 = field1;
 	}
+	/**
+	 * Set Field of the Second table That is Desricbed
+	 * @param field1 Field Name
+	 */
 	public void setField2(final String field2) {
 		this.field2 = field2;
 	}
+
+	/**
+	 * Set tablename of the Second Table this Link Descripton describes 
+	 * @param table1 Field Name
+	 */
 	public void setTable1(final String table1) {
 		this.table1 = table1;
 	}
+	/**
+	 * Set tablename of the Second Table this Link Descripton describes 
+	 * @param table2 Field Name
+	 */
+	
 	public void setTable2(final String table2) {
 		this.table2 = table2;
 	}
