2f4fd0a41bdb74676d79860c8cacdc48ce20952e
pgunia <patrick.gunia@gmail.com>
2013-05-13 15:13:34 +0200

    Added hashCode(), equals()

--- backend/src/main/java/de/uni_koeln/arachne/response/LinkField.java
+++ backend/src/main/java/de/uni_koeln/arachne/response/LinkField.java
@@ -32,4 +32,33 @@ public class LinkField extends Field {
 	public void convertValueToLink() {
 		this.value = "<a href=\"" + this.value + "\">" + this.labelKey + "</a>";
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result
+				+ ((labelKey == null) ? 0 : labelKey.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		LinkField other = (LinkField) obj;
+		if (labelKey == null) {
+			if (other.labelKey != null)
+				return false;
+		} else if (!labelKey.equals(other.labelKey))
+			return false;
+		return true;
+	}
+
+
 }
+
