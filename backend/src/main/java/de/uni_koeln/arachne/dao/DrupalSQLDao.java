2b8e0b9ed4c504276407e85af4738d2b5ffdf27b
Reimar Grabowski <>
2013-06-19 16:14:39 +0200

    Fixed PMD warnings.

--- backend/src/main/java/de/uni_koeln/arachne/dao/DrupalSQLDao.java
+++ backend/src/main/java/de/uni_koeln/arachne/dao/DrupalSQLDao.java
@@ -7,46 +7,48 @@ import de.uni_koeln.arachne.mapping.DatasetMapper;
 
 public class DrupalSQLDao extends SQLDao {
 
-	public Map<String,String> getNode(int nid) {
-		List<Map<String, String>> result = jdbcTemplate.query("SELECT nid, vid, language, type FROM node WHERE nid = ? LIMIT 1",
+	public Map<String,String> getNode(final int nid) {
+		final List<Map<String, String>> result = jdbcTemplate.query("SELECT nid, vid, language, type FROM node WHERE nid = ? LIMIT 1",
 				new Object[]{nid}, new DatasetMapper());
-		if (result != null && !result.isEmpty())
-			return result.get(0);
-		else
+		if (result == null || result.isEmpty()) {
 			return null;
+		} else {
+			return result.get(0);
+		}
 	}
 	
-	public Map<String,String> getRevision(int vid) {
-		List<Map<String, String>> result = jdbcTemplate
+	public Map<String,String> getRevision(final int vid) {
+		final List<Map<String, String>> result = jdbcTemplate
 				.query("SELECT body, title, format FROM node_revisions WHERE vid = ? LIMIT 1",
 						new Object[]{vid}, new DatasetMapper());
-		if (result != null && !result.isEmpty())
+		if (result == null || result.isEmpty()) {
 			return result.get(0);
-		else
+		} else {
 			return null;
+		}
 	}
 	
-	public String getTeaser(int vid) {
+	public String getTeaser(final int vid) {
 		return jdbcTemplate.queryForObject("SELECT field_teaser_value FROM content_type_project WHERE vid = ?",
 				new Object[]{vid}, String.class);
 	}
 	
-	public List<Map<String,String>> getLinks(int vid) {
+	public List<Map<String,String>> getLinks(final int vid) {
 		return jdbcTemplate.query("SELECT field_links_url, field_links_title FROM content_field_links WHERE vid = ? AND field_links_url IS NOT NULL",
 				new Object[]{vid}, new DatasetMapper());
 	}
 	
-	public List<Map<String,String>> getImages(int vid) {
+	public List<Map<String,String>> getImages(final int vid) {
 		return jdbcTemplate.query("SELECT filepath FROM content_field_images JOIN files ON field_images_fid = fid WHERE vid = ?",
 			new Object[]{vid}, new DatasetMapper());
 	}
 
-	public List<Map<String,String>> getMenuEntries(String name) {
+	public List<Map<String,String>> getMenuEntries(final String name) {
 		return jdbcTemplate.query("SELECT mlid, plid, link_path, link_title, has_children FROM menu_links WHERE menu_name LIKE ? ORDER BY weight DESC",
 				new Object[]{name}, new DatasetMapper());	
 	}
 
-	public List<Map<String,String>> getTeasers(String language) {
+	public List<Map<String,String>> getTeasers(final String language) {
 		return jdbcTemplate.query("SELECT nid FROM node WHERE language LIKE ? AND type LIKE 'project' AND promote = 1",
 				new Object[]{language}, new DatasetMapper());	
 	}
