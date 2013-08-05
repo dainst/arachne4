7bc96d729ddffffd6eac634ee68ea0a58d9bbf94
Reimar Grabowski <reimgrab@gmail.com>
2013-08-05 15:56:07 +0200

    Added scheduled dataimport.

--- backend/src/main/java/de/uni_koeln/arachne/controller/AdminController.java
+++ backend/src/main/java/de/uni_koeln/arachne/controller/AdminController.java
@@ -9,6 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -150,5 +151,12 @@ public class AdminController {
 			}
 			return new StatusResponse("Unsupported command.");
 		}
+	}
+	
+	// TODO find a better place for this function - maybe using java spring configuration
+	@Scheduled(fixedDelay=43200000)
+	private void scheduledDataimport() {
+		LOGGER.info("Starting scheduled dataimport...");
+		defaultTaskExecutor.execute(dataImportService);
 	}
 }
