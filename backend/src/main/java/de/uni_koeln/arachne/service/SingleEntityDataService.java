b301128cc8e0236cd4398e6ec3265d7ff828018e
Reimar Grabowski <reimgrab@gmail.com>
2013-08-19 20:59:57 +0200

    Added another line of debugging output.

--- backend/src/main/java/de/uni_koeln/arachne/service/SingleEntityDataService.java
+++ backend/src/main/java/de/uni_koeln/arachne/service/SingleEntityDataService.java
@@ -76,6 +76,7 @@ public class SingleEntityDataService {
 			final String tableName =  entityId.getTableName(); 
 			//If There are Arachne Categories that require the Retrival of other Tables than the table of the Category
 			if ("objekt".equals(tableName) || "buch".equals(tableName)) {
+				LOGGER.debug("Trying to retrieve sub data...");
 				for (final TableConnectionDescription tCD : subProjects) {
 					if(tCD.linksTable(entityId.getTableName())){
 						final Map<String, String> temp = arachneDataMapDao.getBySubDataset(result, tCD);
