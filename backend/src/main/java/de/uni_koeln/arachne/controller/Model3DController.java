ee7192d18eea9dff1f5adc84cf31b2fb23ef2bf0
Reimar Grabowski <reimgrab@gmail.com>
2013-11-12 14:13:44 +0100

    Implemented fetching of 3D meta data from the DB.

--- backend/src/main/java/de/uni_koeln/arachne/controller/Model3DController.java
+++ backend/src/main/java/de/uni_koeln/arachne/controller/Model3DController.java
@@ -11,6 +11,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,9 +24,25 @@ import org.springframework.web.context.support.ServletContextResource;
 import com.google.common.base.Charsets;
 import com.google.common.io.Files;
 
+import de.uni_koeln.arachne.mapping.DatasetGroup;
+import de.uni_koeln.arachne.response.Dataset;
+import de.uni_koeln.arachne.service.EntityIdentificationService;
+import de.uni_koeln.arachne.service.SingleEntityDataService;
+import de.uni_koeln.arachne.service.IUserRightsService;
+import de.uni_koeln.arachne.util.EntityId;
+
 @Controller
 public class Model3DController implements ServletContextAware{
 	private static final Logger LOGGER = LoggerFactory.getLogger(Model3DController.class);
+	
+	@Autowired
+	private transient EntityIdentificationService entityIdentificationService;
+	
+	@Autowired
+	private transient IUserRightsService userRightsService;
+	
+	@Autowired
+	private transient SingleEntityDataService singleEntityDataService;
 	
 	private transient ServletContext servletContext;
 	
@@ -34,25 +51,35 @@ public class Model3DController implements ServletContextAware{
 		this.servletContext = servletContext;		
 	}
 	
-	@RequestMapping(value = "/model/{modelId}", method = RequestMethod.GET)
-	public @ResponseBody String handleModelRequest(@PathVariable("modelId") final Long modelId
+	@RequestMapping(value = "/model/{entityId}", method = RequestMethod.GET)
+	public @ResponseBody String handleModelRequest(@PathVariable("entityId") final Long entityId
 			, @RequestParam(value = "meta", required = false) final Boolean isMeta
 			, final HttpServletRequest request, final HttpServletResponse response) {
-		LOGGER.debug("Request for model: " + modelId + "(" + isMeta + ")");
-				
-		if (isMeta != null && isMeta) {
-			return getMetaData(modelId);
+		
+		final Dataset dataset = getDataset(entityId, response);
+    	
+		if (dataset == null) {
+			return null;
+		}
+		
+    	if (isMeta != null && isMeta) {
+			return getMetaData(dataset);
 		} else {
-			return getModelData(modelId);
+			return getModelData(dataset);
 		}
 	}
 	
-	@RequestMapping(value = "/model/material/{modelId}", method = RequestMethod.GET)
-	public @ResponseBody String handleModelRequest(@PathVariable("modelId") final Long modelId
+	@RequestMapping(value = "/model/material/{entityId}", method = RequestMethod.GET)
+	public @ResponseBody String handleModelRequest(@PathVariable("entityId") final Long entityId
 			, final HttpServletRequest request, final HttpServletResponse response) {
-		LOGGER.debug("Request for material: " + modelId);
-			
-		return getMaterialData(modelId);
+		
+		final Dataset dataset = getDataset(entityId, response);
+    	
+		if (dataset == null) {
+			return null;
+		}
+		
+		return getMaterialData(dataset);
 	}
 	
 	// use regexp workaround for spring truncating at dots in parameters
@@ -64,62 +91,62 @@ public class Model3DController implements ServletContextAware{
 		return texture;
 	}
 
-	private String getMetaData(final long modelId) {
+	private Dataset getDataset(final long entityId, final HttpServletResponse response) {
+		final EntityId arachneId = entityIdentificationService.getId(entityId);
+    	
+    	if (arachneId == null || arachneId.isDeleted() || !arachneId.getTableName().equals("modell3d")) {
+    		response.setStatus(404);
+    		return null;
+    	}
+    	
+    	final String datasetGroupName = singleEntityDataService.getDatasetGroup(arachneId);
+    	final DatasetGroup datasetGroup = new DatasetGroup(datasetGroupName);
+    	if (!userRightsService.userHasDatasetGroup(datasetGroup)) {
+    		response.setStatus(403);
+    		return null;
+    	}
+    	
+    	return singleEntityDataService.getSingleEntityByArachneId(arachneId);
+	}
+	
+	private String getMetaData(final Dataset dataset) {
 		final JSONObject result = new JSONObject();
-		if (modelId == 666) {
-			try {
-				result.put("Titel", "Griechische Vase (TestModel)");
-				result.put("Dateiformat", "objmtl");
-				result.put("Lizenz", "Public Domain");
-				return result.toString();
-			} catch (JSONException e) {
-				// TODO: handle exception
-				LOGGER.error("Failed to generate model meta data. Cause: ", e);
-			}
-		} else {
-			try {
-				result.put("Titel", "Baukomplex der Basilika Aemilia");
-				result.put("Dateiformat", "obj");
-				result.put("Lizenz", "CC-BY-SA 3.0");
-				result.put("connectedEntity", 5640);
-				return result.toString();
-			} catch (JSONException e) {
-				// TODO: handle exception
-				LOGGER.error("Failed to generate model meta data. Cause: ", e);
-			}
+		try {
+			result.put("title", dataset.getFieldFromFields("modell3d.Titel"));
+			result.put("format", dataset.getFieldFromFields("modell3d.Dateiformat"));
+			result.put("license", dataset.getFieldFromFields("modell3d.Lizenz"));
+			return result.toString();
+		} catch (JSONException e) {
+			LOGGER.error("Failed to generate model meta data. Cause: ", e);
 		}
 		return null;
 	}
 
-	private String getModelData(final long modelId) {
-		ServletContextResource modelData = null;
-		if (modelId == 666) {
-			modelData = new ServletContextResource(servletContext, "/WEB-INF/vase.obj");
-		} else {
-			modelData = new ServletContextResource(servletContext, "/WEB-INF/basilika.obj");
-		}
+	private String getModelData(final Dataset dataset) {
+		// TODO read model data from disk ('modell3d.Pfad')
+		final ServletContextResource modelData = new ServletContextResource(servletContext, "/WEB-INF/" 
+					+ dataset.getFieldFromFields("modell3d.Dateiname"));
+		
 		if (modelData.exists()) {
 			try {
 				final File file = modelData.getFile();
-				final String content = Files.toString(file, Charsets.UTF_8);
-				return content;
+				return Files.toString(file, Charsets.UTF_8);
 			} catch (IOException e) {
-				LOGGER.error("Problem reading model file. Caused by: ", e);
+				LOGGER.error("Problem reading model file. Cause: ", e);
 			}
 		}
 		return null;
 	}
 	
-	private String getMaterialData(final long modelId) {
-		ServletContextResource materialData = null;
-		if (modelId == 666) {
-			materialData = new ServletContextResource(servletContext, "/WEB-INF/vase.mtl");
-		} 
+	private String getMaterialData(final Dataset dataset) {
+		// TODO read material data from disk ('modell3d.Pfad')
+		final ServletContextResource materialData = new ServletContextResource(servletContext, "/WEB-INF/" 
+				+ dataset.getFieldFromFields("modell3d.DateinameMTL"));
+		 
 		if (materialData.exists()) {
 			try {
 				final File file = materialData.getFile();
-				final String content = Files.toString(file, Charsets.UTF_8);
-				return content;
+				return Files.toString(file, Charsets.UTF_8);
 			} catch (IOException e) {
 				LOGGER.error("Problem reading model file. Caused by: ", e);
 			}
