eb56f4c67cdada11600e0845a5ec28e0c0f23eb3
clemens <istari@zauberwelten.org>
2013-11-12 09:55:38 +0100

    testcorrection

--- backend/src/test/java/de/uni_koeln/arachne/response/TestTeiViewerSpecialNavigationElement.java
+++ backend/src/test/java/de/uni_koeln/arachne/response/TestTeiViewerSpecialNavigationElement.java
@@ -2,31 +2,45 @@ package de.uni_koeln.arachne.response;
 
 import org.junit.After;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
 
+import de.uni_koeln.arachne.testconfig.WebContextTestExecutionListener;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"classpath:test-context.xml"}) 
+@TestExecutionListeners( { WebContextTestExecutionListener.class,
+	DependencyInjectionTestExecutionListener.class,
+	DirtiesContextTestExecutionListener.class,
+	TransactionalTestExecutionListener.class })
 public class TestTeiViewerSpecialNavigationElement {
 	
+	@Autowired
 	private transient TeiViewerSpecialNavigationElement testItem;
-
-	@Before
-	public void setUp() {
-		testItem = new TeiViewerSpecialNavigationElement();
-	}
-	
-	@After
-	public void teadDown() {
-		testItem = null;
-	}
 	
 	@Test
 	public void testMatches() {
-		Assert.assertTrue(testItem.matches("manifest:antiquitues_of_ionia", null));
+		Assert.assertTrue(testItem.matches("15048", null));
 		Assert.assertFalse(testItem.matches("searchterm", null));
 	}
 	
 	@Test
 	public void testGetResult() {
-		Assert.assertNotNull(testItem.getResult("manifest:antiquitues_of_ionia", null));
+		final boolean matches = testItem.matches("15048", null);
+		
+		Assert.assertTrue(matches);
+		if(matches) {
+			final AbstractSpecialNavigationElement result = testItem.getResult("15048", null);
+			Assert.assertNotNull(result);
+			Assert.assertEquals("http://arachne.uni-koeln.de/Tei-Viewer/cgi-bin/teiviewer.php?manifest=antiquities_of_ionia_1", 
+					result.getLink());
+		}
 	}
 }
