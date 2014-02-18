94613822f7a2656ac1d8f9a7c444afe2005ebcd6
Reimar Grabowski <reimgrab@gmail.com>
2014-02-18 14:15:18 +0100

    Removed unused import.

--- backend/src/main/java/de/uni_koeln/arachne/service/ImageService.java
+++ backend/src/main/java/de/uni_koeln/arachne/service/ImageService.java
@@ -3,7 +3,6 @@ package de.uni_koeln.arachne.service;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 import org.slf4j.Logger;
