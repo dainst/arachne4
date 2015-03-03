00046443d43afa83d76f52ffd4c0df5ee225a134
Reimar Grabowski <reimgrab@gmail.com>
2015-03-03 17:04:24 +0100

    Added basic validation of ES config files.

---
+++ backend/src/test/java/de/uni_koeln/arachne/util/ITestESClientUtil.java
@@ -0,0 +1,92 @@
+package de.uni_koeln.arachne.util;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@RunWith(SpringJUnit4ClassRunner.class) 
+@ContextConfiguration(locations = {"classpath:test-context.xml"}) 
+public class ITestESClientUtil {
+	
+	@Autowired
+	private transient ESClientUtil esClientUtil;
+
+	@Before
+	public void setupContext() {
+		esClientUtil.setServletContext(new MockServletContext("file:src/main/webapp"));
+	}
+	
+	@Test
+	public void validateSetting() {
+		try {
+			final Field settingsFile = ESClientUtil.class.getDeclaredField("SETTINGS_FILE");
+			settingsFile.setAccessible(true);
+			final Method getJsonFromFile = ESClientUtil.class.getDeclaredMethod("getJsonFromFile", String.class);
+			getJsonFromFile.setAccessible(true);
+			final String filename = (String)settingsFile.get(esClientUtil);
+			final String json = (String)getJsonFromFile.invoke(esClientUtil, filename);
+			assertTrue("File 'src/main/webapp" + filename + "' is not valid JSON!", isValidJson(json));
+		} catch (NoSuchFieldException | NoSuchMethodException | SecurityException | IllegalAccessException 
+				| IllegalArgumentException | InvocationTargetException e) {
+			fail(e.toString());
+		}
+	}
+	
+	@Test
+	public void validateMapping() {
+		try {
+			final Field mappingFile = ESClientUtil.class.getDeclaredField("MAPPING_FILE");
+			mappingFile.setAccessible(true);
+			final Method getJsonFromFile = ESClientUtil.class.getDeclaredMethod("getJsonFromFile", String.class);
+			getJsonFromFile.setAccessible(true);
+			final String filename = (String)mappingFile.get(esClientUtil);
+			final String json = (String)getJsonFromFile.invoke(esClientUtil, filename);
+			assertTrue("File 'src/main/webapp" + filename + "' is not valid JSON!", isValidJson(json));
+		} catch (NoSuchFieldException | NoSuchMethodException | SecurityException | IllegalAccessException 
+				| IllegalArgumentException | InvocationTargetException e) {
+			fail(e.toString());
+		}
+	}
+	
+	private boolean isValidJson(final String json) {
+		boolean result = false;
+		JsonParser parser;
+		try {
+			parser = new ObjectMapper().getFactory().createParser(json);
+			while (parser.nextToken() != null) {}
+		    result = true;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}	
+		
+		// check for duplicate keys
+		if (result) {
+			ObjectMapper objectMapper = new ObjectMapper();
+			objectMapper.enable(DeserializationFeature.FAIL_ON_READING_DUP_TREE_KEY);
+			try {
+				objectMapper.readTree(json);
+			} catch (IOException e) {
+				e.printStackTrace();
+				result = false;
+			}
+		}
+		
+		return result;
+	}
+
+}
