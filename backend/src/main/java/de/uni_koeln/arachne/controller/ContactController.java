c76d1a7fb61a6925797a5e34b51d8d2afc0f7b62
Reimar Grabowski <reimgrab@gmail.com>
2015-04-28 15:34:38 +0200

    Fixed constructor to work with Springs depenedency injection.

--- backend/src/main/java/de/uni_koeln/arachne/controller/ContactController.java
+++ backend/src/main/java/de/uni_koeln/arachne/controller/ContactController.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
@@ -30,6 +31,7 @@ public class ContactController {
 	
 	private transient final String contactEmail;
 	
+	@Autowired
 	public ContactController(final @Value("#{config.contactEmail}") String contactEmail) {
 		this.contactEmail = contactEmail;
 	}
