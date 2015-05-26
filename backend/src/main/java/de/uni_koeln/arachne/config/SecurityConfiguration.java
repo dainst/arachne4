4058605d6ae9fa40b4c2f1fc12767a11f297005e
Sebastian Cuy <sebastian.cuy@uni-koeln.de>
2015-05-26 14:24:29 +0200

    disabled CSRF tokens in security config

--- backend/src/main/java/de/uni_koeln/arachne/config/SecurityConfiguration.java
+++ backend/src/main/java/de/uni_koeln/arachne/config/SecurityConfiguration.java
@@ -29,6 +29,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
+			.csrf().disable()
 			.authorizeRequests()
 				.antMatchers(HttpMethod.GET, "/admin/**").hasAnyRole("USER", "ADMIN", "ANONYMOUS")
 				.antMatchers(HttpMethod.POST, "/admin/**").hasRole("ADMIN")
