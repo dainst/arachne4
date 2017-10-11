d3621c8b7dbb0bd1c793d047ead78f1f0cd2ed94
FProx <finn.prox@gmail.com>
2017-10-11 14:16:54 +0200

    Added possibility to include plain text in the xml files. For further documentation take a look at the redmine wiki page

--- backend/src/main/java/de/uni_koeln/arachne/response/ResponseFactory.java
+++ backend/src/main/java/de/uni_koeln/arachne/response/ResponseFactory.java
@@ -312,7 +312,7 @@ public class ResponseFactory {
 					response.addDate(dateAssertion);
 				}
 				final String endEra = link.getFieldFromFields("datierung.EndEpoche");
-				if (!StrUtils.isEmptyOrNull(startEra)) {
+				if (!StrUtils.isEmptyOrNull(endEra)) {
 					final DateAssertion dateAssertion = new DateAssertion(endEra, "Datierung");
 					response.addDate(dateAssertion);
 				}
