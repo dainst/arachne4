44cdda6c27fba1cad4b42a4152f7ed7e2cfcd835
paflov <philipp.franck@dainst.de>
2018-12-18 18:07:09 +0100

    implements a workaround for a bug in jsoup
    which can break the pdf export if some content contains stuff like ´<http://www...´ which could be mistaken as namespace.
    
    closes #10226

--- backend/src/main/java/de/uni_koeln/arachne/export/BasePdfConverter.java
+++ backend/src/main/java/de/uni_koeln/arachne/export/BasePdfConverter.java
@@ -5,6 +5,7 @@ import org.jsoup.Jsoup;
 import org.jsoup.helper.W3CDom;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.w3c.dom.DOMException;
 import org.w3c.dom.Document;
 
 import java.io.IOException;
@@ -44,9 +45,7 @@ public abstract class BasePdfConverter<T> extends AbstractDataExportConverter<T>
     protected void writePdf(StringWriter inStream, OutputStream outStream) {
         PdfRendererBuilder pdfBuilder = new PdfRendererBuilder();
         try {
-            W3CDom w3cDom = new W3CDom();
-            Document w3cDoc = w3cDom.fromJsoup(Jsoup.parse(inStream.toString()));
-
+            Document w3cDoc = convertStringToDocument(inStream.toString());
             pdfBuilder.withW3cDocument(w3cDoc, "/");
             pdfBuilder.toStream(outStream);
             pdfBuilder.run();
@@ -55,6 +54,25 @@ public abstract class BasePdfConverter<T> extends AbstractDataExportConverter<T>
             throw new DataExportException("data_export_io_error", e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
 
+    }
+
+    private Document convertStringToDocument(String content) {
+        W3CDom w3cDom = new W3CDom();
+        try {
+            return w3cDom.fromJsoup(Jsoup.parse(content));
+
+        // this catches an error which occurs, when in the data something is written like ´<http://www´ which can be
+        // mistaken as namespace. This is a known issue in jsoup:
+        // https://github.com/jhy/jsoup/issues/848
+        // it will be fixed in the next version von jsoup and then, this function can be removed.
+        } catch (DOMException e){
+            if (e.code == 14) {
+                LOGGER.debug("namespace_err prevented");
+                content = content.replaceAll("<([\\w\\d]+:)","< $1");
+                return w3cDom.fromJsoup(Jsoup.parse(content));
+            }
+            throw e;
+        }
     }
 
 
