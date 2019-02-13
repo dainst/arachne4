9692c496aa8636adb7a8485423f4b6a826c9bd2a
Reimar Grabowski <reimgrab@gmail.com>
2019-02-13 16:52:27 +0100

    Fixed JavaDoc parameter names.

--- backend/src/main/java/de/uni_koeln/arachne/controller/EntityController.java
+++ backend/src/main/java/de/uni_koeln/arachne/controller/EntityController.java
@@ -67,8 +67,8 @@ public class EntityController {
 	 * Requests for /entity/* return formatted data as JSON.
 	 * @param entityId The unique entity id of the item to fetch.
 	 * @param isLive If the entity shall be fetched from DB (<code>true</code>) or ES (<code>false</code>)
-	 * @param lang The language HTTP request paramter.
-	 * @param headerLanguage The value of the 'Accept-Language' HTTP header.
+	 * @param paramLang The language HTTP request parameter.
+	 * @param headerLang The value of the 'Accept-Language' HTTP header.
      * @return A response object containing the data (this is serialized to JSON).
 	 * @throws Transl8Exception if transl8 cannot be reached. 
      */
@@ -91,8 +91,8 @@ public class EntityController {
      * @param category The database table to fetch the item from.
      * @param categoryId The internal id of the item to fetch.
 	 * @param isLive If the entity shall be fetched from DB (<code>true</code>) or ES (<code>false</code>)
-     * @param lang The language as HTTP parameter.
-     * @param headerLanguage The value of the 'Accept-Language' HTTP header.
+     * @param paramLang The language as HTTP parameter.
+     * @param headerLang The value of the 'Accept-Language' HTTP header.
      * @return A response object containing the data (this is serialized to JSON).
      */
     @RequestMapping(value="/entity/{category}/{categoryId}", 
