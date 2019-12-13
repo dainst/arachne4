c7400ff50b0b0a2416b4e1f7ea1eceefed309a2e
Sebastian Cuy <sebastian.cuy@dainst.de>
2019-12-13 16:01:41 +0100

    Optimize catalog entry endpoint performance (better)
    This change fixes the bug introduced in the last commit. It also makes
    sure that the table index is used in the query for catalog entry
    successors by removing the leading wildcard.

--- backend/src/main/java/de/uni_koeln/arachne/dao/jdbc/CatalogEntryDao.java
+++ backend/src/main/java/de/uni_koeln/arachne/dao/jdbc/CatalogEntryDao.java
@@ -555,7 +555,7 @@ public class CatalogEntryDao extends SQLDao {
 			return;
 		}
 		final String sql = "select count(id) as total_children from catalog_entry where " +
-                "parent_id = " + catalogEntry.getId();
+                "path like '" + catalogEntry.getPath() + "/" + catalogEntry.getId() + "%'";
 		int tmp = queryForInt(sql);
 		catalogEntry.setAllSuccessors(tmp);
 	}
