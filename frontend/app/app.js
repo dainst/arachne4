898bf2162b313be24c8709173524d08de3e18d9d
Sebastian Cuy <sebastian.cuy@dainst.de>
2022-02-07 17:22:40 +0100

    Cleanup imports

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -13,9 +13,6 @@ import './lib/ui-bootstrap-custom-build/ui-bootstrap-custom-2.5.0.js';
 import './lib/ui-bootstrap-custom-build/ui-bootstrap-custom-tpls-2.5.0.js';
 import './lib/ui-bootstrap-custom-build/ui-bootstrap-custom-2.5.0-csp.css';
 
-import 'leaflet.markercluster/dist/MarkerCluster.css';
-import 'leaflet.markercluster/dist/MarkerCluster.Default.css';
-
 import transl8_en from './_transl8.en.js';
 import transl8_de from './_transl8.de.js';
 
