1ece8bc99d91a35ce8a9b9a1b61bf93017445b58
Sebastian Cuy <sebastian.cuy@dainst.de>
2022-02-07 10:31:08 +0100

    Fix imagegrid in categories

--- frontend/app/category/category.module.js
+++ frontend/app/category/category.module.js
@@ -1,7 +1,8 @@
 import CategoriesController from './categories.controller.js';
 import CategoryController from './category.controller.js';
+import imageModule from '../image/image.module.js';
 
-export default angular.module('arachne.category', [])
+export default angular.module('arachne.category', [imageModule.name])
     .config(['$stateProvider', $stateProvider => {
         $stateProvider.state({ name: 'categories', url: '/categories', template: require('./categories.html')});
         $stateProvider.state({ name: 'category',  url: '/category/?c&facet&fv&group', template: require('./category.html')});
