927369126088535dc8fdd48cc86ddb5ed0b78f5e
Sebastian Cuy <sebastian.cuy@dainst.de>
2022-02-25 10:31:01 +0100

    Remove unused IndexController from module definition

--- frontend/app/index/index.module.js
+++ frontend/app/index/index.module.js
@@ -1,10 +1,5 @@
-import IndexController from './index.controller.js';
 import indexService from './index.service.js';
 
 export default angular.module('arachne.index', [])
-    .config(['$stateProvider', $stateProvider => {
-        $stateProvider.state({ name: 'index', url: '/index?c&fq&fv&group', template: require('./index.html') });
-    }])
-    .controller('IndexController', ['$scope', 'categoryService', 'Entity', 'Query', '$stateParams', '$location', 'indexService', IndexController])
     .factory('indexService', ['$filter', 'Entity', '$http', 'Query', '$q', indexService])
 ;
