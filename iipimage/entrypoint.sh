b7b5f804a35c304c289d2ca5d7e2f672ff043e9f
Sebastian Cuy <sebastian.cuy@dainst.de>
2022-03-10 15:38:52 +0100

    Fix iipimage entrypoint

--- iipimage/entrypoint.sh
+++ iipimage/entrypoint.sh
@@ -2,30 +2,45 @@
 
 export VERBOSITY=2
 export MAX_IMAGE_CACHE_SIZE=0
+
 FASTCGI_USER=www-data
 FASTCGI_GROUP=www-data
 IIPSRV=/usr/lib/iipimage-server/iipsrv.fcgi
 
+# tells the shell to exit if any of the foreground command fails
+set -eu
+
 echo "Starting IIPImage"
 
+pids=()
+
 # spawn iip instance without watermarking
-SOCKET="$SOCKET_PATH/iip-fastcgi.socket"
-spawn-fcgi -u $FASTCGI_USER -g $FASTCGI_GROUP -f $IIPSRV -a $(hostname -I) -p 9000
+spawn-fcgi -u $FASTCGI_USER -g $FASTCGI_GROUP -f $IIPSRV -a $(hostname -I) -p 9000 -n &
+pids+=($!)
 
 # spawn iip instances with watermarks
 export WATERMARK_PROBABILITY="1"
 export WATERMARK_OPACITY="0.1"
 
-export WATERMARK="/usr/lib/cgi-bin/watermarks/watermark_arachne.tif"
-spawn-fcgi -u $FASTCGI_USER -g $FASTCGI_GROUP -f $IIPSRV -a $(hostname -I) -p 9001
+export WATERMARK="/watermarks/watermark_arachne.tif"
+spawn-fcgi -u $FASTCGI_USER -g $FASTCGI_GROUP -f $IIPSRV -a $(hostname -I) -p 9001 -n &
+pids+=($!)
 
-export WATERMARK="/usr/lib/cgi-bin/watermarks/watermark_berlin.tif"
-spawn-fcgi -u $FASTCGI_USER -g $FASTCGI_GROUP -f $IIPSRV -a $(hostname -I) -p 9002
+export WATERMARK="/watermarks/watermark_berlin.tif"
+spawn-fcgi -u $FASTCGI_USER -g $FASTCGI_GROUP -f $IIPSRV -a $(hostname -I) -p 9002 -n &
+pids+=($!)
 
-export WATERMARK="/usr/lib/cgi-bin/watermarks/watermark_dai.tif"
-spawn-fcgi -u $FASTCGI_USER -g $FASTCGI_GROUP -f $IIPSRV -a $(hostname -I) -p 9003
+export WATERMARK="/watermarks/watermark_dai.tif"
+spawn-fcgi -u $FASTCGI_USER -g $FASTCGI_GROUP -f $IIPSRV -a $(hostname -I) -p 9003 -n &
+pids+=($!)
 
-export WATERMARK="/usr/lib/cgi-bin/watermarks/watermark_oppenheim.tif"
-spawn-fcgi -u $FASTCGI_USER -g $FASTCGI_GROUP -f $IIPSRV -a $(hostname -I) -p 9004
+export WATERMARK="/watermarks/watermark_oppenheim.tif"
+spawn-fcgi -u $FASTCGI_USER -g $FASTCGI_GROUP -f $IIPSRV -a $(hostname -I) -p 9004 -n &
+pids+=($!)
 
-wait
+echo $pids
+
+# ait for each specific process to terminate.
+for pid in "${pids[@]}"; do
+  wait "$pid"
+done
