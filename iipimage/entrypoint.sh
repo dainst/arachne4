643ec20ea98219b01422efa2226fce5bd6c1549f
Sebastian Cuy <sebastian.cuy@dainst.org>
2022-03-10 14:54:10 +0000

    Fix iipimage entrypoint

--- iipimage/entrypoint.sh
+++ iipimage/entrypoint.sh
@@ -7,16 +7,10 @@ FASTCGI_USER=www-data
 FASTCGI_GROUP=www-data
 IIPSRV=/usr/lib/iipimage-server/iipsrv.fcgi
 
-# tells the shell to exit if any of the foreground command fails
-set -eu
-
 echo "Starting IIPImage"
-
-pids=()
 
 # spawn iip instance without watermarking
 spawn-fcgi -u $FASTCGI_USER -g $FASTCGI_GROUP -f $IIPSRV -a $(hostname -I) -p 9000 -n &
-pids+=($!)
 
 # spawn iip instances with watermarks
 export WATERMARK_PROBABILITY="1"
@@ -24,23 +18,19 @@ export WATERMARK_OPACITY="0.1"
 
 export WATERMARK="/watermarks/watermark_arachne.tif"
 spawn-fcgi -u $FASTCGI_USER -g $FASTCGI_GROUP -f $IIPSRV -a $(hostname -I) -p 9001 -n &
-pids+=($!)
 
 export WATERMARK="/watermarks/watermark_berlin.tif"
 spawn-fcgi -u $FASTCGI_USER -g $FASTCGI_GROUP -f $IIPSRV -a $(hostname -I) -p 9002 -n &
-pids+=($!)
 
 export WATERMARK="/watermarks/watermark_dai.tif"
 spawn-fcgi -u $FASTCGI_USER -g $FASTCGI_GROUP -f $IIPSRV -a $(hostname -I) -p 9003 -n &
-pids+=($!)
 
 export WATERMARK="/watermarks/watermark_oppenheim.tif"
 spawn-fcgi -u $FASTCGI_USER -g $FASTCGI_GROUP -f $IIPSRV -a $(hostname -I) -p 9004 -n &
-pids+=($!)
 
-echo $pids
+# Wait for any process to exit
+wait -n
 
-# ait for each specific process to terminate.
-for pid in "${pids[@]}"; do
-  wait "$pid"
-done
+# Exit with status of process that exited first
+exit $?
+
