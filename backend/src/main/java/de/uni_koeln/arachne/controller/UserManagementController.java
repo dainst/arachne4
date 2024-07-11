de8a7737b24de293381853b703faa130377e6113
Simon Hohl <simon.hohl@dainst.de>
2024-07-11 16:50:00 +0200

    Remove mentions of oneeyedjacks

--- backend/src/main/java/de/uni_koeln/arachne/controller/UserManagementController.java
+++ backend/src/main/java/de/uni_koeln/arachne/controller/UserManagementController.java
@@ -300,7 +300,7 @@ public class UserManagementController {
 						+ "E-Mail: " + user.getEmail() + newLine + newLine
 						+ "Wenn Sie in Arachne eingeloggt sind, können Sie folgenden Link benutzen um den Benutzer "
 						+ "freizuschalten:" + newLine
-						+ "https://oneeyedjacks04.dai-cloud.uni-koeln.de/activate_account/" + user.getId();
+						+ "https://https://objects.legacy.idai.world/activate_account/" + user.getId();
 
 				if (!mailService.sendMail(adminEmail, "Anmeldung bei Arachne", messageBody)) {
 					LOGGER.error("Unable to send registration eMail to admin.");
