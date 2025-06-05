23084b9d675316820fdfe469fb422db62ae339b5
Simon Hohl <simon.hohl@dainst.de>
2025-06-05 10:21:31 +0200

    Create database backup script
    The script splits the user table data into a separate dump.

--- database_backups.sh
+++ database_backups.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
-# This script will create an arachne database dump.
+# This script will create an arachne database backup by first exporting
+# the whole database excluding the `verwaltung_benutzer` data, then a second
+# export with only the table `verwaltung_benutzer`.
+# Parameters:
+#   $1: Path to target directory
 
 # Read .env file and use as environment variables
 export $(cat .env | sed 's/#.*//g' | xargs)
 
-BACKUP_DIR="/mnt/cephfs/idai_world/backups/idai_objects"
+
+if [ -n "$1" ]
+then
+	BACKUP_DIR=$1
+else
+	BACKUP_DIR="/opt/backup"
+fi
+
+echo "Backing up to $BACKUP_DIR."
 
 docker exec arachne4_db sh -c "mariadb-dump -uroot -p${DB_ROOT_PASSWORD} --ignore-table-data=arachne.verwaltung_benutzer arachne" > $BACKUP_DIR/arachne_dump.sql
 gzip -c $BACKUP_DIR/arachne_dump.sql > $BACKUP_DIR/arachne_dump_$(date +"%Y_%m_%d").gz
